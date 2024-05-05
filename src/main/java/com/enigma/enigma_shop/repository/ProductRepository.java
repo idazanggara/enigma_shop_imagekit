2899adc5245775255d7e27ed58de9bbd0dc8acb1
idazanggara <idazideas@gmail.com>
2024-05-05 22:18:07 +0700

    Merge remote-tracking branch 'github/01.Service-Pattern' into 01.Service-Pattern

--- src/main/java/com/enigma/enigma_shop/repository/ProductRepository.java
+++ src/main/java/com/enigma/enigma_shop/repository/ProductRepository.java
@@ -12,6 +12,8 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     // find by name -< tidak boleh sembarangan buat nama
     // Optional<T> findBy... -> result List/Satuan
     // Stream<T>/List<T> findAllBy.. ->  List
+    // contoh lain
+    // List<Product> findAllByNameAndPrice(String name, Long price)
     // karena name spesifik, jadi kita pakai query like
     List<Product> findAllByNameLike(String name);
 }
